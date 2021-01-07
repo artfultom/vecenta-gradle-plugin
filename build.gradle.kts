a3b1972071b093117f997d56b82c40fafbf8be02
Andrey Vasilyev <artfultomcat@gmail.com>
2021-01-07 23:48:04 +0300

    [Add] gradle

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `java-gradle-plugin`
+    `maven-publish`
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+}
+
+group = "my.artfultom.vecenta.tools"
+version = "0.0.1"
+
+tasks.withType<Wrapper> {
+    gradleVersion = "6.7.1"
+}
