fe9f814bac4a8c1b360eabbc8e3fbe1e680d0cfe
Andrey Vasilyev <artfultom@protonmail.com>
2022-01-07 15:40:37 +0300

    [Add] input vars

--- src/main/kotlin/my/artfultom/vecenta/tools/VecentaPlugin.kt
+++ src/main/kotlin/my/artfultom/vecenta/tools/VecentaPlugin.kt
@@ -1,9 +1,14 @@
 package my.artfultom.vecenta.tools
 
+import my.artfultom.vecenta.generate.Configuration
+import my.artfultom.vecenta.generate.DefaultCodeGenerateStrategy
+import my.artfultom.vecenta.generate.FileGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import java.nio.file.Paths
 
 class VecentaPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -13,8 +18,27 @@ class VecentaPlugin : Plugin<Project> {
 
 open class Generate : DefaultTask() {
 
+    @Input
+    var schemaDir: String? = null
+
+    @Input
+    var targetDir: String? = null
+
+    @Input
+    var serverPackage: String? = null
+
+    @Input
+    var clientPackage: String? = null
+
     @TaskAction
     fun generate() {
-        TODO("Not yet implemented")
+        val strategy = DefaultCodeGenerateStrategy()
+        val config = Configuration(
+                Paths.get(schemaDir.toString()),
+                Paths.get(targetDir.toString()),
+                serverPackage,
+                clientPackage
+        )
+        FileGenerator(strategy).generateFiles(config)
     }
 }
